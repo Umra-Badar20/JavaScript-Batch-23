9bd5c03561c6f448f32befa402f27d7147cdd471
Umra-Badar20 <umrabadar555@gmail.com>
2026-08-27 17:57:52 +0500

    chap 2

---
+++ app.js
@@ -0,0 +1,17 @@
+// alert("Umra") //String
+// alert(10) //Number
+// alert(true); //boolean
+
+// console.log("Umra")
+// console.log(10)
+// console.log(false)
+
+// var institute = "SMIT"
+// alert(institute)
+// console.log(institute);
+// alert(institute)
+
+var userName;
+console.log(userName);
+userName = "Umra"
+console.log(userName);
