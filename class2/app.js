8adc0b610374a1eb6d4c2815405f754c39b088e3
Umra-Badar20 <umrabadar555@gmail.com>
2026-08-28 17:45:15 +0500

    Chap 5

---
+++ class2/app.js
@@ -0,0 +1,25 @@
+// var authorName ="Umra badar"
+// var author = "Umra's bag"
+// var string = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias dignissimos iure doloribus totam quasi "incidunt atque, quo corporis" perferendis quidem sapiente deleniti quas repudiandae! Tenetur laborum fugit harum in fugiat.'
+// var num1 = 10
+// var num2 = 20
+// var add = num1+num2 
+// console.log(num1,"+", num2, "=", add);
+// document.write(num1,"+", num2, "=", add);
+// 10 + 20 = 30
+
+// var myNum =  50
+// var secNum = 20
+// console.log(myNum);
+// myNum = myNum + secNum
+// console.log(myNum);
+
+// alert(myNum)
+
+var my_Name = "Umra" // snake case
+var myName = "Badar" // Camel case
+
+//Question 1
+// alert("Welcome to SMIT")
+
+// Question 2
