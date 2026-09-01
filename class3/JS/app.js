2804375d1ee7ab967a01eeea3a495cd78b19c7fb
Umra-Badar20 <umrabadar555@gmail.com>
2026-09-01 17:55:10 +0500

    chap 9

---
+++ class3/JS/app.js
@@ -0,0 +1,66 @@
+// alert(10)
+// alert("Umra")
+
+// var userName = "Umra Badar"
+// document.write(userName)
+// userName ="Aniqa"
+// console.log(userName);
+
+
+// var num = "100"
+// var newNum = num / 19 //100 + 20
+// console.log(newNum);
+
+
+// var num1 = 10
+// // num1 = num1 + 1
+// // num1++
+// // num1++
+// // num1++
+// console.log(num1--); //10
+// console.log(num1); //9
+
+
+// var number = 36
+// console.log(++number); //37
+// console.log(number); //37
+
+
+
+
+// console.log(num++ + num + --num); //10 + 11 + 10 =31
+
+// var num = 10
+// console.log(num++ + ++num + --num + num--); //10 + 12 + 11 +11
+// console.log(--num - num++ - num-- - num); //9 - 9 - 10 -9 
+
+
+// var a = 2, b =1
+// var result = --a - --b + ++b + b-- //1 - 0 + 1 + 1
+// console.log(result);
+
+
+// console.log( 1 + 3 * 4); //16  13
+
+// var user = "Umra Badar"
+// var string = "Welcome Umra, to my Website!"
+// var str = "Welcome "+ user+", to my website"
+// console.log(str);
+
+
+// console.log(2+2+"Hello"+ 2 +2);
+
+
+// var user = prompt("Hello, Enter your name here!")
+// console.log("welcome "+user);
+
+// var gender = prompt("Enter your gender!", "Female")
+// console.log(gender);
+
+
+
+// var num = +prompt("Enter a number")
+// console.log(num );
+
+var user ="Umra Badar"
+document.write(" <h1>"+user+"</h1>")
