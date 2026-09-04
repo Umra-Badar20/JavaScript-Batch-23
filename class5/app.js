0c93e86ba113e4fe2c005a0b14a60926bcb7785c
Umra-Badar20 <umrabadar555@gmail.com>
2026-09-04 17:44:06 +0500

    chap 13

---
+++ class5/app.js
@@ -0,0 +1,67 @@
+console.log(1);
+// var age = prompt("Enter your age here:")
+// if(age >= 18){
+//     console.log("You are eligible");
+// }else{
+//     console.log("You are under 18");
+// }
+// console.log(3);
+// var age = prompt("Enter your age here:")
+// if(age <= 18){
+//     console.log("You are eligible");
+// }else{
+//     console.log("You are over 18");
+// }
+// console.log(3);
+
+
+// var userName = "Umra"
+// if(userName !== "Umra"){
+//     console.log("Welcome to smit");
+// }else{
+//     console.log("You are Umra");
+// }
+
+
+// var planet = prompt("In which planet do you live?")
+
+// if(planet !== "earth"){
+//     console.log("wrong answer");
+// }else{
+//     console.log("correct answer");
+
+// }
+
+// var age = Number(prompt("enter your age"))
+// var gender = prompt("enter your gender")
+
+// if(age <=18 && gender === "female"){
+//     console.log("You are eligible");
+// }else{
+//     console.log("You are not eligible");
+
+// }
+// if(age <=18 || gender === "female"){
+//     console.log("You are eligible");
+// }else{
+//     console.log("You are not eligible");
+
+// }
+// if((age <=18 || age >=25) && gender === "female"){
+//     console.log("You are eligible");
+// }else{
+//     console.log("You are not eligible");
+
+// }
+var num = 11
+if(num == 10){
+    console.log("correct guess!");
+}else if(num == 9){
+    console.log("You are close");
+}else if(num == 11){
+    console.log("Minimize your number");
+}
+else{
+    console.log("wrong guess!");
+
+}
