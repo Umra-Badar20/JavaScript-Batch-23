3164a8fba1645c01388e21ff07d503cbc21d81e7
Umra-Badar20 <umrabadar555@gmail.com>
2026-09-17 18:01:33 +0500

    chap 19

---
+++ class10/app.js
@@ -0,0 +1,61 @@
+// for(var i = 0; i<=10; i++ ){ //i=3 ; 10<=10
+//     console.log(i,"Umra");
+
+// }
+
+
+// for(var i = 1; i<=100; i++ ){ //i=3 ; 10<=10
+//     console.log(`2 X ${i} = ${i*2}`);
+
+// }
+
+
+
+// var arr = ["apple", "mango", "grapes","banana","kiwi","orange"] //5
+// for(var i =0; i<arr.length ; i++){// i =4 ; 4<5
+//     console.log(arr[i],"is a fruit"); //arr[3]
+// }
+
+// var userFruit= prompt("Enter a fruit name") //banana
+
+// var flag = false
+
+// var arr = ["apple", "mango", "grapes","banana","kiwi","orange"] 
+// for(var i =0; i<arr.length ; i++){
+//    if(userFruit == arr[i]){
+//     flag = true
+//     console.log(userFruit,"is available");
+//    }
+// }
+// if(flag ==  false){
+//     console.log(userFruit,"is not available");
+// }
+
+
+// var user = "Umra"
+// console.log(user);
+// user = "Sundus"
+// console.log(user);
+
+
+
+// for(var i=10; i>= 0; i--){
+//     console.log(i);
+// }
+
+
+// for(var i=1; i<=20; i++){
+//     if(i%2 !==0){
+//         console.log(i);
+//     }
+// }
+//  even
+for(var i=0; i<=20; i+=2){
+// i++
+console.log(i)
+}
+// odd
+for(var i=0; i<=20; i++){
+    i++
+    console.log(i)
+    }
